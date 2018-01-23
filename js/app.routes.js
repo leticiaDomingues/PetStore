66e742984d67b0c95c595db31b55e2ee406cd7fa
Letícia Domingues <leticia.domingues@opus-software.com.br>
2018-01-23 15:06:11 -0200

    Structure - Main page

---
+++ js/app.routes.js
@@ -0,0 +1,12 @@
+angular
+	.module('app')
+	.config(config);
+
+config.$inject = ['$routeProvider'];
+
+function config($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'view/pets.html'
+		});	
+}
