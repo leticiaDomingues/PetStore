2f4d1134c92b9b6baf683de3b8a4f259e09985e5
Letícia Domingues <leticia.domingues@opus-software.com.br>
2018-01-24 14:41:43 -0200

    Home page - pet detail modal

--- js/controllers/pets.js
+++ js/controllers/pets.js
@@ -10,7 +10,7 @@
 	function petsController(PetsService) {
 		var self = this;
 
-		self.pets = PetsService.query({status: 'disponivel'});
+		self.pets = PetsService.query({findByStatus: 'findByStatus', status: 'disponivel'});
 		self.pets.$promise.then(function() {
 			self.pets.forEach(function(pet) { pet.photoIndex = 0 });
 		}, function() {});
@@ -19,6 +19,16 @@
 
 		self.changePetThumbnail = function(index) {
 			self.pets[index].photoIndex++;
+		}
+
+		self.openPetDetailsModal = function(index) {
+			self.clickedPet = self.pets[index];
+			console.log(self.clickedPet);
+			$('#petDetailsModal').modal('toggle');
+		}
+
+		self.closePetDetailsModal = function() {
+			$('#petDetailsModal').modal('hide')
 		}
 	}
 })();
