a737bed930efdd4a4babfe7c1faf2c9055b4bd3f
Letícia Domingues <leticia.domingues@opus-software.com.br>
2018-01-31 14:41:25 -0200

    Signup page - validation and refactoring (login validation)

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -5,47 +5,56 @@
 		.module('app')
 		.controller('LoginController', loginController);
 
-	loginController.$inject = ['$scope','PetsHttp',  '$location', 'ValidatorService', '$localStorage' ,'$routeParams'];
+	loginController.$inject = ['$scope','PetsHttp',  '$location', '$localStorage' ,'$routeParams'];
 
-	function loginController($scope, PetsHttp, $location, ValidatorService, $localStorage, $routeParams) {
+	function loginController($scope, PetsHttp, $location, $localStorage, $routeParams) {
 		var self = this;
-		self.errors = ValidatorService.errors;
-		self.errors.incorrectCredentials = { display: 'none'};
+
+		//validation variables
+		self.incorrectCredentials = { display: 'none'};
+		self.ngDirtyWhenSubmitted="";
+
+		//location variable
 		self.previousPage = ($routeParams.previousPage) ? $routeParams.previousPage : "home";
 
+
+		//user object
 		self.user = {
 			username : "",
 			password : ""
 		}
 
+		//redirect to home if user is already logged-in
 		if($localStorage.user)
 			$location.path('/');
 
 		self.logar = function() {
-			if(ValidatorService.validadeFields(self.user)) {
-				let promise = PetsHttp.login(self.user);
+			let promise = PetsHttp.login(self.user);
 
-				//success - user found
-				promise.then(function(success) {
-					if(success) {
-						//variavel de sessao do user
-						$localStorage.user = success;
-						//atualiza a view
-						$scope.$parent.$parent.currentUser = $localStorage.user;
+			//success - user found
+			promise.then(function(success) {
+				if(success) {
+					//variavel de sessao do user
+					$localStorage.user = success;
+					//atualiza a view
+					$scope.$parent.$parent.currentUser = $localStorage.user;
 
-						if(self.previousPage=='home')
-							$location.path('/');
-						else if(self.previousPage.indexOf('makeOrder') > -1)
-							$location.path('/makeOrder/' + self.previousPage.substring(9));
-					} else {
-						self.errors.incorrectCredentials = { display: 'block'};
-					}
-			    }, 
-			    //error - user not found
-			    function(){
-		            self.errors.incorrectCredentials = { display: 'block'};
-		        }); 				
-			}
+					if(self.previousPage=='home')
+						$location.path('/');
+					else if(self.previousPage.indexOf('makeOrder') > -1)
+						$location.path('/makeOrder/' + self.previousPage.substring(9));
+				} else {
+					self.incorrectCredentials = { display: 'block'};
+				}
+		    }, 
+		    //error - user not found
+		    function(){
+	            self.incorrectCredentials = { display: 'block'};
+	        }); 				
+		};
+
+		self.dirtyWhenSubmitted = function() {
+			self.ngDirtyWhenSubmitted="ng-dirty";
 		};
 	}
 })();
--- js/controllers/signup.js
+++ js/controllers/signup.js
@@ -5,12 +5,15 @@
 		.module('app')
 		.controller('SignupController', signupController);
 
-	signupController.$inject = ['$scope','PetsHttp',  '$location', 'ValidatorService', '$localStorage'];
+	signupController.$inject = ['$scope','PetsHttp',  '$location', '$localStorage'];
 
-	function signupController($scope, PetsHttp, $location, ValidatorService, $localStorage) {
+	function signupController($scope, PetsHttp, $location, $localStorage) {
 		var self = this;
-		self.signupError = { display: 'none'};
 
+		//internal error and username taken error
+		self.errors = [{ display: 'none'},{ display: 'none'}];
+
+		//user object
 		self.user = {
 			firstName : "",
 			lastName : "",
@@ -19,29 +22,43 @@
 			password : ""
 		}
 
+		//firstName, lastName, email, username, password
+		self.maxLength = [30,30,50,20,20];
+		self.ngDirtyWhenSubmitted="";
+
 		//redirect to the home page if user is already logged-in
 		if($localStorage.user)
 			$location.path('/');
 
 		self.signup = function() {
-			console.log(self.user);
+			//check if the username is already taken
+			let promise = PetsHttp.login(self.user);
 
-			let promise = PetsHttp.signup(self.user);
+			//username taken
+			promise.then(function() { 
+				self.errors[1] = { display: 'block'};
+			}, 
+		    //username not taken
+		    function(){ 
+		    	let promise = PetsHttp.signup(self.user);
 
-			promise.then(function(data) {
-				//put user in the session variable
-				$localStorage.user = data.config.data;
-				//update main view (username)
-				$scope.$parent.$parent.currentUser = $localStorage.user;
+				promise.then(function(data) {
+					//put user in the session variable
+					$localStorage.user = data.config.data;
+					//update main view (username)
+					$scope.$parent.$parent.currentUser = $localStorage.user;
 
-				//redirect to home 
-				$location.path('/');
-		    }, function(data){
-		    	self.signupError = { display: 'block'};
-	            console.log(data);
-	        }); 				
-			
+					//redirect to home 
+					$location.path('/');
+			    }, function(data){
+			    	self.errors[0] = { display: 'block'};
+		        }); 
+		    }); 
 		};
+
+		self.dirtyWhenSubmitted = function() {
+			self.ngDirtyWhenSubmitted="ng-dirty";
+		}
 
 		self.cleanFields = function() {
 			self.user.firstName = "";
